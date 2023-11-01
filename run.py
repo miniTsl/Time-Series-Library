e66c405c1dc7a8b6fd0a088323a6d45f1f8cf1aa
miniTsl <1668685245@qq.com>
2023-11-01 16:27:53 +0800

    准备去moon执行

--- run.py
+++ run.py
@@ -88,7 +88,7 @@ if __name__ == '__main__':
     parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
     parser.add_argument('--gpu', type=int, default=0, help='gpu')
     parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
-    parser.add_argument('--devices', type=str, default='0,1,2,3', help='device ids of multile gpus')
+    parser.add_argument('--devices', type=str, default='0,1', help='device ids of multile gpus')
 
     # de-stationary projector params
     parser.add_argument('--p_hidden_dims', type=int, nargs='+', default=[128, 128],
@@ -124,7 +124,7 @@ if __name__ == '__main__':
     if args.is_training:
         for ii in range(args.itr):
             # setting record of experiments
-            setting = '{}_{}_{}_{}_ft{}_sl{}_ll{}_pl{}_dm{}_nh{}_el{}_dl{}_df{}_fc{}_eb{}_dt{}_{}_{}'.format(
+            setting = '{}_{}_{}_{}_ft{}_sl{}_ll{}_pl{}_dm{}_nh{}_el{}_dl{}_df{}_fc{}_eb{}_dt{}_topk{}_{}_{}'.format(
                 args.task_name,
                 args.model_id,
                 args.model,
@@ -141,6 +141,7 @@ if __name__ == '__main__':
                 args.factor,
                 args.embed,
                 args.distil,
+                args.top_k,
                 args.des, ii)
 
             exp = Exp(args)  # set experiments
--- scripts/server.sh
+++ scripts/server.sh
@@ -1,29 +1,34 @@
-export CUDA_VISIBLE_DEVICES=0
-
+# export CUDA_VISIBLE_DEVICES=0,1
 model_name=TimesNet
-
-python -u run.py \
-  --task_name long_term_forecast \
-  --is_training 0 \
-  --model_id Server_300_300 \
-  --model $model_name \
-  --data Server \
-  --features S \
-  --seq_len 300 \
-  --label_len 100\
-  --root_path ./dataset/ETT-small/\
-  --data_path ETTh1.csv\
-  --pred_len 300 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 1 \
-  --dec_in 1 \
-  --c_out 1 \
-  --d_model 16 \
-  --d_ff 32 \
-  --des 'Exp' \
-  --itr 1 \
-  --top_k 5 \
-  --batch_size 64 \
-  --inverse
+ks=(1 2 3 4 5)
+for k in ${ks[@]}
+do
+  python -u run.py \
+    --task_name long_term_forecast \
+    --is_training 0 \
+    --model_id Server_300_300 \
+    --model $model_name \
+    --data Server \
+    --features S \
+    --seq_len 300 \
+    --label_len 100\
+    --root_path ./dataset/ETT-small/\
+    --data_path ETTh1.csv\
+    --train_epochs 20\
+    --use_multi_gpu \
+    --is_training 1 \
+    --pred_len 300 \
+    --e_layers 2 \
+    --d_layers 1 \
+    --factor 3 \
+    --enc_in 1 \
+    --dec_in 1 \
+    --c_out 1 \
+    --d_model 16 \
+    --d_ff 32 \
+    --des 'Exp' \
+    --itr 1 \
+    --top_k $k \
+    --batch_size 64 \
+    --inverse
+done
